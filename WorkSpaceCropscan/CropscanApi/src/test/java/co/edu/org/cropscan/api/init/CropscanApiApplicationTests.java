8cab8114d4f8b41279325e0a3634648e6313d811
Helmer Alexander Patiño <80782571+1193090526alexander@users.noreply.github.com>
2023-06-06 00:10:37 -0500

    Create of Java Project

---
+++ WorkSpaceCropscan/CropscanApi/src/test/java/co/edu/org/cropscan/api/init/CropscanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.org.cropscan.api.init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropscanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
