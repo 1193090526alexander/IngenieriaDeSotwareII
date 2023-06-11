39528ad60db348a0aaf3c3e667f67a329bddecb9
Juanjose0017 <110146287+Juanjose0017@users.noreply.github.com>
2023-06-11 17:59:50 -0500

    Creacion de paquetes dto y entity

---
+++ CropScanSpace/CropscanEntity/src/co/edu/org/cropscan/entity/TipoDocumentoEntity.java
@@ -0,0 +1,5 @@
+package co.edu.org.cropscan.entity;
+
+public class TipoDocumentoEntity {
+
+}
---
+++ CropScanSpace/CropscanEntity/src/co/edu/org/cropscan/entity/UsuarioEntity.java
@@ -0,0 +1,10 @@
+package co.edu.org.cropscan.entity;
+
+import java.util.UUID;
+
+
+
+public class UsuarioEntity {
+	private UUID codigo; 
+
+}
