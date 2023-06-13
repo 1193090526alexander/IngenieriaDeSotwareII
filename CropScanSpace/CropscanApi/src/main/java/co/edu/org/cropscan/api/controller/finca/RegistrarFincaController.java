9f5907c9cc73b8e1683bbd9c2c3f809f5362d7fe
Helmer Alexander Patiño <80782571+1193090526alexander@users.noreply.github.com>
2023-06-13 10:31:57 -0500

    controller

---
+++ CropScanSpace/CropscanApi/src/main/java/co/edu/org/cropscan/api/controller/finca/RegistrarFincaController.java
@@ -0,0 +1,26 @@
+package co.edu.org.cropscan.api.controller.finca;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import co.edu.org.cropscan.dto.FincaDTO;
+import co.edu.org.cropscan.service.facade.finca.RegistrarFincaUseCaseFacade;
+
+@RestController
+@RequestMapping("cropscan/api/v1/finca")
+public class RegistrarFincaController {
+	
+	@Autowired
+	private RegistrarFincaUseCaseFacade facade;
+
+	@PostMapping
+	public ResponseEntity<String> execute(@RequestBody FincaDTO finca){
+		facade.execute(finca);
+		return new ResponseEntity<>("Éxito", HttpStatus.ACCEPTED);
+		
+	}
+}
