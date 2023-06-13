44ab863635b92003ebf3a858792b260214df0f13
Helmer Alexander Patiño <80782571+1193090526alexander@users.noreply.github.com>
2023-06-12 19:25:59 -0500

    Creacion de Fron End

---
+++ webapp/indexPrincipal/usuario.php
@@ -0,0 +1,20 @@
+<?php 
+session_start();
+
+session_unset();
+
+session_destroy();
+
+header('location: ')
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1>bienvenido usuario</h1>
+</body>
+</html>
