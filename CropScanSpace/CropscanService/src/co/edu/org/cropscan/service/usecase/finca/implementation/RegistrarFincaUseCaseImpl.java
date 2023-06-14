b12244a710706c36cd0de31e5ba646b851db9b7d
Juanjose0017 <110146287+Juanjose0017@users.noreply.github.com>
2023-06-14 13:31:26 -0500

    Implementation Usuario y Finca

--- CropScanSpace/CropscanService/src/co/edu/org/cropscan/service/usecase/finca/implementation/RegistrarFincaUseCaseImpl.java
+++ CropScanSpace/CropscanService/src/co/edu/org/cropscan/service/usecase/finca/implementation/RegistrarFincaUseCaseImpl.java
@@ -3,9 +3,11 @@ package co.edu.org.cropscan.service.usecase.finca.implementation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import co.edu.org.cropscan.entity.FincaEntity;
 import co.edu.org.cropscan.entity.TipoDocumentoEntity;
 import co.edu.org.cropscan.repository.FincaRepository;
 import co.edu.org.cropscan.repository.TipoDocumentoRepository;
+import co.edu.org.cropscan.service.assembler.ModelMapper;
 import co.edu.org.cropscan.service.domain.FincaDomain;
 import co.edu.org.cropscan.service.domain.TipoDocumentoDomain;
 import co.edu.org.cropscan.service.usecase.finca.RegistrarFincaUseCase;
@@ -16,13 +18,22 @@ public class RegistrarFincaUseCaseImpl implements RegistrarFincaUseCase {
 
 	@Autowired
 	private FincaRepository repository;
-
+	
+	private final ModelMapper<FincaDomain, FincaEntity> modelmapper;
+	
+	public RegistrarFincaUseCaseImpl(FincaRepository repository, ModelMapper<FincaDomain, FincaEntity> modelmapper) {
+        this.repository = repository;
+        this.modelmapper = modelmapper;
+	}
 	@Override
 	public void execute(FincaDomain domain) {
-		// TODO Auto-generated method stub
 		
+		//crear las reglas de negocio
+		//specification pattern o un validator pattern
+		//Aqui deberas crear el ensamblador
+		
+		FincaEntity entity = null;
+		repository.save(entity);
 	}
-	
+}	
 
-
-}
