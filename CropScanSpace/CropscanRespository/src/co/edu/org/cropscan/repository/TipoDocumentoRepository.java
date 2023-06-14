c3fa808eb0778b61431424107b836fd13ed0aacf
Helmer Alexander Patiño <80782571+1193090526alexander@users.noreply.github.com>
2023-06-13 22:33:18 -0500

    controller

--- CropScanSpace/CropscanRespository/src/co/edu/org/cropscan/repository/TipoDocumentoRepository.java
+++ CropScanSpace/CropscanRespository/src/co/edu/org/cropscan/repository/TipoDocumentoRepository.java
@@ -2,7 +2,6 @@ package co.edu.org.cropscan.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import co.edu.org.cropscan.entity.TipoDocumentoEntity;
 
 
--- CropScanSpace/CropscanService/src/co/edu/org/cropscan/service/usecase/usuario/implementation/ListarUsuarioUseCasesImpl.java
+++ CropScanSpace/CropscanService/src/co/edu/org/cropscan/service/usecase/usuario/implementation/ListarUsuarioUseCasesImpl.java
@@ -5,24 +5,24 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import co.edu.org.cropscan.repository.TipoDocumentoRepository;
-import co.edu.org.cropscan.service.domain.FincaDomain;
+import co.edu.org.cropscan.repository.UsuarioRepository;
 import co.edu.org.cropscan.service.domain.UsuarioDomain;
-import co.edu.org.cropscan.service.usecase.finca.ListarFincaUseCase;
+
 import co.edu.org.cropscan.service.usecase.usuario.ListarUsuarioUseCase;
 
 @Service
 public class ListarUsuarioUseCasesImpl implements ListarUsuarioUseCase {
 
 	@Autowired
-	private TipoDocumentoRepository repository;
+	private UsuarioRepository repository;
 
 	@Override
 	public List<UsuarioDomain> execute(Optional<UsuarioDomain> domain) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+
 
 
 
