7f3d5fb4b2a3c9bf2686dec28ac73ef2d3906f05
Juanjose0017 <110146287+Juanjose0017@users.noreply.github.com>
2023-06-14 21:17:14 -0500

    Sendgrid

--- CropScanSpace/CropscanApi/src/main/java/co/edu/org/cropscan/api/controller/sendgrid/EmailController.java
+++ CropScanSpace/CropscanApi/src/main/java/co/edu/org/cropscan/api/controller/sendgrid/EmailController.java
@@ -7,28 +7,42 @@ import com.sendgrid.helpers.mail.objects.*;
 import java.io.IOException;
 
 public class EmailController {
+	
+	private final SendGrid toApi;
+	
+	public EmailController(){
+		this.toApi = new SendGrid("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");
+	}
+	
+	public void sendMail (){
+		
+		Email from = new Email("elmerp1193090526@gmail.com");
+	    Email to = new Email("juan.cardona4185@uco.net.co");
 
-   public static void main(String[] args) throws IOException {
+	    String subject = "Welcome to CropScan";
+	    Content content = new Content("text/plain", "Bienvenido ");
 
-       Email from = new Email("elmerp1193090526@gmail.com");
-       Email to = new Email("juanjose01212@gmail.com");
+	    Mail mail = new Mail(from, subject, to, content);
 
-       String subject = "Sending with Twilio SendGrid is Fun";
-       Content content = new Content("text/html", "and <em>easy</em> to do anywhere with <strong>Java</strong>");
+	    SendGrid sg = new SendGrid(System.getenv("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"));
+	    Request request = new Request();
+	    
+	    try {
+	    	request.setMethod(Method.POST);
+		    request.setEndpoint("mail/send");
+		    request.setBody(mail.build());
 
-       Mail mail = new Mail(from, subject, to, content);
+		    Response response = sg.api(request);
 
-       SendGrid sg = new SendGrid(System.getenv("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"));
-       Request request = new Request();
-
-       request.setMethod(Method.POST);
-       request.setEndpoint("mail/send");
-       request.setBody(mail.build());
-
-       Response response = sg.api(request);
-
-       System.out.println(response.getStatusCode());
-       System.out.println(response.getHeaders());
-       System.out.println(response.getBody());
-   }
+		    System.out.println(response.getStatusCode());
+		    System.out.println(response.getHeaders());
+		    System.out.println(response.getBody());
+	    	
+	    }catch(IOException e){
+	    	
+	    }
+	    
+	    	
+	}
+   
 }
